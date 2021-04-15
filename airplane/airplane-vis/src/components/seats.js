ba17d8eed9e41e01887a996051d817c771c259a5
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-04-15 02:08:19 -0400

    Added plane layout

--- airplane/airplane-vis/src/components/seats.js
+++ airplane/airplane-vis/src/components/seats.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./seats.css";
 import Image from "./download1.png";
 import Test from "./test1.png";
+import Plane from "./plane1.png";
 
 const BOARD_SIZE = 11;
 const rowSize = 3;
@@ -65,54 +66,67 @@ const NewBoard = () => {
     <>
       <span className="body">
         <button onClick={onCellClick}>Test</button>
-        <div className="dacols">
-          {board.map((row, rowIdx) => (
-            <div key={rowIdx} className="row">
-              {/* {alpArray[rowIdx]} */}
 
-              {row.map((cell, cellIdx) => (
-                <div
-                  key={cellIdx}
-                  className={
-                    boardIndeces[rowIdx][cellIdx] === 0
-                      ? "inactive_class"
-                      : "cell_Clicked"
-                  }
-                >
-                  <img
-                    src={boardIndeces[rowIdx][cellIdx] === 0 ? Image : Test}
-                    width="35.69px"
-                    height="44.69px"
-                  ></img>
+        <div style={{ position: "relative" }}>
+          <img src={Plane} width="1490px" height="740px" className="ima"></img>
+          <div
+            style={{
+              position: "absolute",
+              top: "175px",
+              left: "0",
+              zIndex: "5",
+            }}
+          >
+            <div className="dacols">
+              {board.map((row, rowIdx) => (
+                <div key={rowIdx} className="row">
+                  {/* {alpArray[rowIdx]} */}
+
+                  {row.map((cell, cellIdx) => (
+                    <div
+                      key={cellIdx}
+                      className={
+                        boardIndeces[rowIdx][cellIdx] === 0
+                          ? "inactive_class"
+                          : "cell_Clicked"
+                      }
+                    >
+                      <img
+                        src={boardIndeces[rowIdx][cellIdx] === 0 ? Image : Test}
+                        width="35.69px"
+                        height="44.69px"
+                      ></img>
+                    </div>
+                  ))}
                 </div>
               ))}
             </div>
-          ))}
-        </div>
-        <div className="space"></div>
-        <div className="dacols">
-          {board.map((row, rowIdx) => (
-            <div key={rowIdx} className="row">
-              {/* {alpArray[rowIdx]} */}
+            <div className="space"></div>
+            <div className="dacols">
+              {board.map((row, rowIdx) => (
+                <div key={rowIdx} className="row">
+                  {/* {alpArray[rowIdx]} */}
 
-              {row.map((cell, cellIdx) => (
-                <div
-                  key={cellIdx}
-                  className={
-                    boardIndeces[rowIdx][cellIdx] === 0
-                      ? "inactive_class"
-                      : "cell_Clicked"
-                  }
-                >
-                  <img
-                    src={boardIndeces[rowIdx][cellIdx] === 0 ? Image : Test}
-                    width="35.69px"
-                    height="44.69px"
-                  ></img>
+                  {row.map((cell, cellIdx) => (
+                    <div
+                      key={cellIdx}
+                      className={
+                        boardIndeces[rowIdx][cellIdx] === 0
+                          ? "inactive_class"
+                          : "cell_Clicked"
+                      }
+                    >
+                      <img
+                        src={boardIndeces[rowIdx][cellIdx] === 0 ? Image : Test}
+                        width="35.69px"
+                        height="44.69px"
+                      ></img>
+                    </div>
+                  ))}
                 </div>
               ))}
             </div>
-          ))}
+          </div>
         </div>
       </span>
     </>
